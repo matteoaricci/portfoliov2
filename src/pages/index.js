a5d555a899046f3960744dd3500f766dc730069a
matteoaricci <matteo.a.ricci@gmail.com>
2020-07-30 15:15:13 -0400

    header formatted

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,12 @@
 import React from "react"
 import '../styles/styles.scss'
+
+// Import Components
+import Header from '../components/Header'
 const IndexPage = () => (
-  <p>Home</p>
+  <div>
+    <Header />
+  </div>
 )
 
 export default IndexPage
